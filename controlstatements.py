3a33f7169e9c8bba2af2e5eca863ea637fc5ddc4
PeterKariuki47 <ppeterkkariuki47@gmail.com>
2023-07-18 11:32:58 -0700

    if-elif-else

--- controlstatements.py
+++ controlstatements.py
@@ -1,6 +1,14 @@
 age = input("Enter Your age  ")
 print("Your age is "+age)
-if age > "18":
-    print("You are an Adult")
+if age <= "12":
+    print("You are a child")
+elif age < "19":
+    print("You are a child")
+elif age < "35":
+    print("You are a youth")
+elif age < "50":
+    print("You are an adult")
+elif age < "70":
+    print("You are a senior member of the society")
 else:
-    print("You are a minor")    
+    print("You are Elderly")    
