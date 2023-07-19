3de3fd926cd20dc9655525ad31cd99735ba81ca8
PeterKariuki47 <ppeterkkariuki47@gmail.com>
2023-07-19 01:17:08 -0700

    Nested functions

--- functions.py
+++ functions.py
@@ -1,8 +1,13 @@
-#Variable scope
+#Nested functions
 
-age=8
-def test():
-    print(age)
+def count():
+    count = 0
 
-print(age)
-test()
+    def increment():
+        nonlocal count # the keyword "nonlocal" is used to call a variable that is outside the function but in another function
+        count = count + 1
+        print(count)
+    increment()
+
+count() 
+
