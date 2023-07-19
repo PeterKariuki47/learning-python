a84efe9c1d3b95475787befbfb69e9a2ada5ab08
PeterKariuki47 <ppeterkkariuki47@gmail.com>
2023-07-19 02:41:32 -0700

    Inheritance in Python

--- classes.py
+++ classes.py
@@ -1,5 +1,5 @@
 # Classes
-
+"""
 class Dog:
     def __init__(self, name, age):
         self.name = name
@@ -13,3 +13,34 @@ Dog1 = Dog("Rodger", 9)
 print(Dog1.name)
 print(Dog1.age)
 Dog1.bark()
+
+"""
+
+#Inheritance in Python
+
+
+class Animal:
+    def __init__ (self, name, habitat):
+        self.name = name
+        self.habitat = habitat
+
+    def walk(self):
+        return "Walking..."
+    
+
+class Dog(Animal):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+    def bark(self):
+        print("Woof!")
+
+Dog1 = Dog("Rodger", 9)
+print(Dog1.name)
+print(Dog1.age)
+Dog1.bark()
+print(Dog1.walk())
+
+
