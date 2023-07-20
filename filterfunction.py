7d07aa6d77ff032ddca5513241a5c1f285f715c2
PeterKariuki47 <ppeterkkariuki47@gmail.com>
2023-07-19 20:46:06 -0700

    Filtering using a lambda function
    modified the code using a lambda function

--- filterfunction.py
+++ filterfunction.py
@@ -6,7 +6,14 @@ def isEven(n):
     if n % 2 == 0:
         return True
     else:
-        return False
+        return False 
 
 result = filter(isEven, numbers)
+print(list(result))
+
+# Modified code using a lambda function
+
+numbers = [11,12,13,14,15,16,17,18,19]
+
+result= filter(lambda n:n%2==0, numbers)
 print(list(result))
