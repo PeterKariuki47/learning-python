72779cc0f316f25d5474add5b8a4198600b0c703
PeterKariuki47 <ppeterkkariuki47@gmail.com>
2023-07-19 21:03:19 -0700

    Reduce functions in python
    Solved a problem using the reduce function

---
+++ reducefunction.py
@@ -0,0 +1,27 @@
+# reduce functions
+from functools import reduce
+
+"""
+expenses = [("Dinner", 80),
+            ("Car repair", 120)
+]
+
+for expense in expenses:
+    sum = 0
+    sum+= expense[1]
+    print(sum)
+
+
+"""
+
+
+#the reduce function in practice 
+#imported the reduce function on top
+
+
+expenses = [("Dinner", 80),
+            ("Car repair", 120)
+]
+
+sum = reduce(lambda a,b: a[1] + b[1], expenses)
+print(sum)
