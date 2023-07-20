8ced8a48b0a5b4773e0f4dfcb649a5f58a2a873d
PeterKariuki47 <ppeterkkariuki47@gmail.com>
2023-07-19 20:31:45 -0700

    Mapping a lambda function
    using a lambda function to simplify code

--- mapfunction.py
+++ mapfunction.py
@@ -7,4 +7,11 @@ def double(a):
 
 result=map(double,numbers)
 print(list(result))
-    
+
+
+#simplifying the code using a lambda function
+
+numbers = [1,2,3,4,5]
+
+result = map(lambda a:a*2, numbers)
+print(list(result))
